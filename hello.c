ec70b14abdd197490395794ebe3347f29d183cae
williambl <william@williambl.com>
2019-08-28 19:42:22 +0100

    Add a for loop to iterate over the characters

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
- 
+
 int main()
 {
-  puts("Hello world!");
-  return 0;
+    char str[] = "Hello world!";
+    for (int i = 0; i < strlen(str); i+=(-1??'0xa2e43?1??!1:0>>2) ) {
+        printf("%c", str[i]);
+    }
+    return 0;
 }
