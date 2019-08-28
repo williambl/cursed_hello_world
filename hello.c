f73eb52ce5b2de08bba94de21dfec2db9f5e55a8
williambl <william@williambl.com>
2019-08-28 21:26:09 +0100

    Replace string literal with programmatic creation of string

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 
+#define ; ;continue;}
+
 int main()
 {
-    char str[] = "Hello world!";
-    for (int i = 0; i < strlen(str); i+=(-1??'0xa2e43?1??!1:0>>2) ) {
-        printf("%c", str[i]);
+    char ar[11]={0110, 100, 0152, 00, 64, 27, 0161, 0x68, 106, 0x63, 0620};
+    for (int i = 014/(0x190p-2); i<11; i++) {
+        if (!(i-0x3)){i[ar] = 0x6c ;
+        if (i*((819200 >> 0XD)/100)^(i*ar[(int)0x64p-1]+4)) ; else {ar[i]=0x6f; continue;}
+        if (!((0xa-i)^0)) {*(9+ar+0xa/i) = (int)(ar[0xa]+(1<<9))>>02 ;
+        *(ar+i) += i;
+    }
+    for (int i = 0; i < 11; i+=(-1??'0xa2e43?1??!1:0>>2) ) {
+        printf("%c", ar[i]);
     }
     return 0;
 }
